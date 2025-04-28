aea774d129a6e62f1d90db6a06c33294f1f2fa38
jimxshaw <jimxshaw@gmail.com>
2025-04-28 13:35:01 -0500

    Add PivotStrategy enum.

--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,2 +1,8 @@
 public class QuickSorter {
+    public enum PivotStrategy {
+        FIRST_ELEMENT,
+        RANDOM_ELEMENT,
+        MEDIAN_OF_THREE_RANDOM_ELEMENTS,
+        MEDIAN_OF_THREE_ELEMENTS
+    }
 }
