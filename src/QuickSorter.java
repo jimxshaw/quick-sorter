0f0e8e33a05ca7900b19472f7a66938f67029c90
jimxshaw <jimxshaw@gmail.com>
2025-05-04 22:02:21 -0500

    Add comment.

--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -7,7 +7,8 @@ public class QuickSorter {
     public enum PivotStrategy {
         FIRST_ELEMENT,
         RANDOM_ELEMENT,
-        MEDIAN_OF_THREE_RANDOM_ELEMENTS,
+        //MEDIAN_OF_THREE_RANDOM_ELEMENTS, // NOT IMPLEMENTED.
+
         // First, Middle, Last
         MEDIAN_OF_THREE_ELEMENTS
     }
